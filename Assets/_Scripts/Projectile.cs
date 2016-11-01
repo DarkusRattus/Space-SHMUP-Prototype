e6dcef81712fadeb99b738dd2bde05a9b34cb452
DarkusRattus <kristian.benson@rutgers.edu>
2016-11-01 11:04:02 -0400

    Added some Projectile code

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    private WeaponType _type;
+    
+    // This public property masks the field _type and takes action when it is set
+    public WeaponType type
+    {
+        get
+        {
+            return (_type);
+        }
+        set
+        {
+            SetType(value);
+        }
+    }
+    
+    void Awake()
+    {
+        // Test to see whether this has passed off screen every 2 seconds
+        InvokeRepeating("CheckOffscreen", 2f, 2f);
+    }
+
+    public void SetType(WeaponType eType)
+    {
+        _type = eType;
+        WeaponDefinition def = Main.GetWeaponDefinition(_type);
+        GetComponent<Renderer>().material.color = def.projectileColor;
+    }
+
+    void CheckOffscreen()
+    {
+        if (Utils.ScreenBoundsCheck(GetComponent<Collider>().bounds, BoundsTest.offScreen) != Vector3.zero)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
 }
